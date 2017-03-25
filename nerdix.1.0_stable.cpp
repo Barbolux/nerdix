d2ce806a4c466dbc85783c5e53c55b3e00ef19fd
Barbolux <tefoz@gmx.com>
2017-03-25 18:39:06 -0300

    Add files via upload

---
+++ nerdix.1.0_stable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout << "._______________.\n";
+cout << "| N E R D I X   |\n";
+cout << "| E   |   |     |\n";
+cout << "| R --|---|--   |\n";
+cout << "| D   |   |     |\n";
+cout << "| I --|---|--   |\n";
+cout << "| X   |   |     |\n";
+cout << "|_______________|\n";
+	return 0;
+}
